758744f446954d7a0ad79e56056323ba0805b79d
RobertJLupinek <9854828608@GSA.GOV>
2019-12-30 16:18:51 -0500

    Adding s3 bucket logging enablement check.

--- example/main.tf
+++ example/main.tf
@@ -1,8 +1,9 @@
 module "config" {
   source         = "../"
   config_bucket_name         = "${var.config_bucket_name}"
-  aws_account_id = "${var.aws_account_id}"
-  aws_region     = "${var.aws_region}"
-  project     = "${var.project}"  
+  aws_account_id    = "${var.aws_account_id}"
+  aws_region        = "${var.aws_region}"
+  project           = "${var.project}"  
+  access_bucket     = "${var.project}"  
 }
 
